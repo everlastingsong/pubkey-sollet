89bb5ce4318af0c1a7fcf3dd326929d5eb56cd4f
yugure <109891005+yugure-orca@users.noreply.github.com>
2025-07-17 15:34:33 +0900

    popup tweak

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -40,34 +40,67 @@ function restoreConfig() {
 function restoreLastTransactions() {
   chrome.storage.local.get("lastTransactions", function({lastTransactions}) {
     const lastTransactionsFrame = document.getElementById('last-transactions-frame');
-    const innerHTML = `<pre>${JSON.stringify(lastTransactions, null, 2)}</pre>`;
-    lastTransactionsFrame.innerHTML = innerHTML;
+
+    lastTransactions.forEach((tx) => {
+      const transactionDiv = document.createElement('div');
+
+      const versionDiv = document.createElement('div');
+      transactionDiv.appendChild(versionDiv);
+      const sizeDiv = document.createElement('div');
+      transactionDiv.appendChild(sizeDiv);
+      const instructionsTable = document.createElement('table');
+      transactionDiv.appendChild(instructionsTable);
+
+      versionDiv.innerText = `Version: ${tx.version}`;
+      sizeDiv.innerText = `Size: ${tx.serialized.length}`;
+
+      let instructionsTableInnerHTML = "";
+      tx.instructions.forEach((ix, i) => {
+        instructionsTableInnerHTML +=
+        `
+        <tr><td rowspan="3">${i}.</td><td colspan="2"><pre>${ix.programId}</pre></td></tr>
+        <tr><td>Keys</td><td><pre>${ix.keys.map((k, i) => `${("[" + i + "]").padEnd(4, " ")} r${k.isWritable ? 'w' : '-'}${k.isSigner ? 's' : '-'} ${k.pubkey}`).join('\n')}</pre></td></tr>
+        <tr><td>Data</td><td><pre style="white-space: pre-wrap; margin-bottom: 10pt;">${ix.data.map(d => d.toString(16).padStart(2, '0')).join(' ')}</pre></td></tr>
+        `;
+      });
+
+      instructionsTable.className = "instructions-table";
+      instructionsTable.innerHTML = instructionsTableInnerHTML;
+
+      lastTransactionsFrame.appendChild(transactionDiv);
+    });
+
+    if (lastTransactions.length > 0) {
+      selectTab('tx');
+    }
   });
 }
 
-function buildPopup() {
+function selectTab(tab) {
   const configBtn = document.getElementById('config-btn');
   const txBtn = document.getElementById('tx-btn');
   const configSection = document.getElementById('config-section');
   const txSection = document.getElementById('tx-section');
 
-  function selectTab(tab) {
-    if (tab === 'config') {
-      configBtn.classList.add('selected');
-      txBtn.classList.remove('selected');
-      configSection.classList.remove('hidden');
-      txSection.classList.add('hidden');
-    } else {
-      configBtn.classList.remove('selected');
-      txBtn.classList.add('selected');
-      configSection.classList.add('hidden');
-      txSection.classList.remove('hidden');
-    }
+  if (tab === 'config') {
+    configBtn.classList.add('selected');
+    txBtn.classList.remove('selected');
+    configSection.classList.remove('hidden');
+    txSection.classList.add('hidden');
+  } else {
+    configBtn.classList.remove('selected');
+    txBtn.classList.add('selected');
+    configSection.classList.add('hidden');
+    txSection.classList.remove('hidden');
   }
+}
+
+function buildPopup() {
+  const configBtn = document.getElementById('config-btn');
+  const txBtn = document.getElementById('tx-btn');
 
   configBtn.addEventListener('click', function() { selectTab('config'); });
   txBtn.addEventListener('click', function() { selectTab('tx'); });
-
   selectTab('config');
 
   document.getElementById('save').addEventListener('click', saveConfig);
